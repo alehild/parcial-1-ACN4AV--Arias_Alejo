bd5538b9cb5c79558a847e7d3fc60b800ed78506
alehild <167775505+alehild@users.noreply.github.com>
2024-09-28 17:39:22 -0300

    feat: toast notificacion guardar receta

--- app/src/main/java/com/alejodep/libreriacocina/MainActivity.java
+++ app/src/main/java/com/alejodep/libreriacocina/MainActivity.java
@@ -1,13 +1,13 @@
 package com.alejodep.libreriacocina;
 
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,10 +16,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        Button botonGuardar = findViewById(R.id.bot_guardar);
+
+
+        botonGuardar.setOnClickListener(v -> {
+            Toast.makeText(getApplicationContext(), "Receta guardada", Toast.LENGTH_SHORT).show();
         });
     }
 }
