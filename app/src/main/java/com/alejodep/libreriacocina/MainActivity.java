2d96ee82f384d8d9b96e7b1b77624df06899a0f8
alehild <167775505+alehild@users.noreply.github.com>
2024-09-28 18:56:03 -0300

    feat: add scollview and linearlayout

--- app/src/main/java/com/alejodep/libreriacocina/MainActivity.java
+++ app/src/main/java/com/alejodep/libreriacocina/MainActivity.java
@@ -1,9 +1,13 @@
 package com.alejodep.libreriacocina;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -11,13 +15,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTextIngredients;
+    private EditText editTextRecipeName;
+    private EditText editTextRecipeTime;
+    private LinearLayout linearLayoutIngredients;
+    private Button buttonAddIngredient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        // Inicialización de los views
+        editTextIngredients = findViewById(R.id.editTextIngredients);
+        editTextRecipeName = findViewById(R.id.editTextRecipeName);
+        editTextRecipeTime = findViewById(R.id.editTextRecipeTime);
+        linearLayoutIngredients = findViewById(R.id.LayoutIngredients);
+        buttonAddIngredient = findViewById(R.id.buttonAddIngredient);
 
+
+        // Gestión de las barras del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -25,11 +42,28 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        Button botonGuardar = findViewById(R.id.bot_guardar);
-
-
-        botonGuardar.setOnClickListener(v -> {
-            Toast.makeText(getApplicationContext(), "Receta guardada", Toast.LENGTH_SHORT).show();
+        // Evento para el botón "Agregar Ingrediente
+        buttonAddIngredient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ingredient = editTextIngredients.getText().toString();
+                if (!ingredient.isEmpty()) {
+                    addIngredientToList(ingredient);
+                    editTextIngredients.setText("");
+                } else {
+                    Toast.makeText(MainActivity.this, "Por favor, ingresa un ingrediente", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+
+
     }
+
+    private void addIngredientToList(String ingredient) {
+        TextView textView = new TextView(this);
+        textView.setText(ingredient);
+        linearLayoutIngredients.addView(textView);
+    }
+
+
 }
